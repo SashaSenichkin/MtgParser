85ecf3d4f310e965a5614f178a77b56fa9c95fff
Aleksandr Senickin <aleksandr_senickin@senla.eu>
2022-07-14 11:37:31 +0300

    init

---
+++ MtgParser/MtgParser/CardInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using AngleSharp.Dom;
+
+namespace MtgParser;
+
+public class CardInfo
+{
+    public string Title;
+    public string CardType;
+
+    public string SummonCost;
+    public string Str;
+    public string Rarity;
+    public string EngDescr;
+    public string RusDescr;
+
+    public CardInfo( IHtmlCollection<IElement> cellsText,  IHtmlCollection<IElement> cellsInfo)
+    {
+        Title = cellsInfo[0].TextContent;
+        CardType = cellsInfo[1].TextContent;
+        SummonCost = cellsInfo[2].TextContent;
+        Str = cellsInfo[3].TextContent;
+        Rarity = cellsInfo[4].TextContent;
+        
+        EngDescr = cellsText[0].TextContent;
+        RusDescr = cellsText[1].TextContent;
+    }
+    
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        sb.Append($"title = {Title}");
+        sb.Append($"cardType = {CardType}");
+        sb.Append($"summonCost = {SummonCost}");
+        sb.Append($"str = {Str}");
+        sb.Append($"rarity = {Rarity}");
+        sb.Append($"engDescr = {EngDescr}");
+        sb.Append($"rusDescr = {RusDescr}");
+        return sb.ToString();
+    }
+}
--- MtgParser/MtgParser/Program.cs
+++ MtgParser/MtgParser/Program.cs
@@ -1,3 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using AngleSharp;
+using AngleSharp.Dom;
 
-Console.WriteLine("Hello, World!");
+namespace MtgParser;
+
+
+class Program
+{
+    private const string baseUrl = "http://www.mtg.ru/cards/search.phtml?Title=";
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            var doc = await GetCardInfo(args);
+            var result = ParceDoc(doc);
+            File.WriteAllText("test.txt", result.ToString());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+    }
+
+    private static CardInfo ParceDoc(IDocument doc)
+    {
+        string cellSelectorMain = ".SearchCardInfoText";
+        IHtmlCollection<IElement> cellsText = doc.QuerySelectorAll(cellSelectorMain);
+        
+        string cellSelectorInfo = ".SearchCardInfoDIV";
+        IHtmlCollection<IElement> cellsInfo = doc.QuerySelectorAll(cellSelectorInfo);
+
+        return new CardInfo(cellsText: cellsText, cellsInfo: cellsInfo);
+    }
+
+    private static async Task<IDocument> GetCardInfo(string[] args)
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        IConfiguration config = Configuration.Default.WithDefaultLoader();
+        IBrowsingContext context = BrowsingContext.New(config);
+        return await context.OpenAsync(baseUrl + args[0]);
+    }
+}
