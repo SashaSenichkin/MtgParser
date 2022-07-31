5a587b17455bcc2249bffef9a96915e0fb493bd9
admin <111@111.ru>
2022-07-31 14:49:26 +0300

    add rusTranslation to card

--- MtgParser/MtgParser/Models/Card.cs
+++ MtgParser/MtgParser/Models/Card.cs
@@ -7,6 +7,7 @@ public class Card : BaseModel
     public string Cmc { get; set; }
     public string Type { get; set; }
     public string Text { get; set; }
+    public string TextRus { get; set; }
     public string Power { get; set; }
     public string Toughness { get; set; }
     public string Img { get; set; }
