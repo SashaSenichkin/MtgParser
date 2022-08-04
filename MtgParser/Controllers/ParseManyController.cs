a3888a2336b60b3ea273d4e8958117068d6e5fd0
admin <111@111.ru>
2022-08-05 01:55:40 +0300

    first attempt does't work yet

---
+++ MtgParser/Controllers/ParseManyController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MtgParser.Context;
+using MtgParser.Model;
+using MtgParser.ParseLogic;
+
+namespace MtgParser.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ParseManyController : ControllerBase
+{
+    private readonly ParseService _parseService;
+    private readonly MtgContext _dbContext;
+
+
+    public ParseManyController(MtgContext dbContext, ParseService parseService)
+    {
+        _parseService = parseService;
+        _dbContext = dbContext;
+    }
+    
+    
+    [HttpPost(Name = "PostMany")]
+    public async Task<bool> PostToDb()
+    {
+        try
+        {
+            var source = _dbContext.CardsNames;
+            foreach (CardName cardRequest in source)
+            {
+                Card card = await _parseService.ParseOneCard(cardRequest.Name);
+                await _dbContext.Cards.AddAsync(card);
+            }
+            
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return false;
+        }
+    }
+}
