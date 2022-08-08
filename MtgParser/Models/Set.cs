891dcecae8d655e94c921361309044f17a5ae239
admin <111@111.ru>
2022-08-08 20:32:00 +0300

    #4 lil migration

--- MtgParser/Models/Set.cs
+++ MtgParser/Models/Set.cs
@@ -2,7 +2,7 @@ namespace MtgParser.Model;
 
 public class Set : BaseModel
 {
-    public string RusName { get; set; }
+    public string? RusName { get; set; }
     public string FullName { get; set; }
     public string ShortName { get; set; }
     public string SearchText { get; set; }
