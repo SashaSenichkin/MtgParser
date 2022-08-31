2287ecaa9e8d5e60193a5a14df68a7c6e3a799ed
Aleksandr Senickin <aleksandr_senickin@senla.eu>
2022-08-31 13:27:09 +0300

    follow the modern style, yo

--- MtgParser/Program.cs
+++ MtgParser/Program.cs
@@ -2,55 +2,44 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using MtgParser.Context;
 using MtgParser.ParseLogic;
 using Serilog;
 
-namespace MtgParser;
 
-public class Program
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+        
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services)
+    .Enrich.FromLogContext()
+    .WriteTo.Console());
+        
+builder.Services.AddScoped<ParseService>();
+
+string? connectionString = builder.Configuration.GetConnectionString("MtgContext");
+ServerVersion? version = ServerVersion.AutoDetect(connectionString);
+builder.Services.AddDbContext<MtgContext>(options => options.UseMySql(connectionString, version));
+        
+WebApplication app = builder.Build();
+        
+CheckAndUpdateDb(app);
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseSerilogRequestLogging();   
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
+
+
+void CheckAndUpdateDb(IApplicationBuilder app)
 {
-
-    public static void Main(string[] args)
-    {
-        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddControllers();
-
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-        
-        builder.Host.UseSerilog((context, services, configuration) => configuration
-            .ReadFrom.Configuration(context.Configuration)
-            .ReadFrom.Services(services)
-            .Enrich.FromLogContext()
-            .WriteTo.Console());
-        
-        builder.Services.AddScoped<ParseService>();
-
-        string? connectionString = builder.Configuration.GetConnectionString("MtgContext");
-        ServerVersion? version = ServerVersion.AutoDetect(connectionString);
-        builder.Services.AddDbContext<MtgContext>(options => options.UseMySql(connectionString, version));
-        
-        WebApplication app = builder.Build();
-        
-        CheckAndUpdateDb(app);
-
-        app.UseSwagger();
-        app.UseSwaggerUI();
-        app.UseSerilogRequestLogging();
-        
-        app.UseHttpsRedirection();
-
-        app.MapControllers();
-        app.Run();
-    }
-
-    private static void CheckAndUpdateDb(IApplicationBuilder app)
-    {
-        using IServiceScope scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
-        scope.ServiceProvider.GetService<MtgContext>()?.Database.Migrate();
-    }
+    using IServiceScope scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
+    scope.ServiceProvider.GetService<MtgContext>()?.Database.Migrate();
 }
