50ef6a6182294e34b4ae8cffa38da6b0aab17d31
admin <111@111.ru>
2022-09-01 21:20:17 +0300

    Init database with rarities and keywords

--- MtgParser/Context/MtgContext.cs
+++ MtgParser/Context/MtgContext.cs
@@ -15,4 +15,39 @@ public class MtgContext : DbContext
     public DbSet<Card> Cards { get; set; }
     public DbSet<CardSet> CardsSets { get; set; }
     public DbSet<CardName> CardsNames { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Rarity>().HasData
+        (
+            new Rarity { Id = 1, Name = "common", RusName = "обычная"},
+            new Rarity { Id = 2, Name = "uncommon", RusName = "необычная"},
+            new Rarity { Id = 3, Name = "rare", RusName = "редкая"},
+            new Rarity { Id = 4, Name = "mythic", RusName = "раритетная"},
+            new Rarity { Id = 5, Name = "special", RusName = "специальная"}
+        );
+        
+        modelBuilder.Entity<Keyword>().HasData
+        (
+            new Keyword { Id = 1, Name = "Deathtouch", RusName = "Смертельное касание"},
+            new Keyword { Id = 2, Name = "Defender", RusName = "Защитник"},
+            new Keyword { Id = 3, Name = "Double strike", RusName = "Двойной удар"},
+            new Keyword { Id = 4, Name = "Enchant", RusName = "Зачаровать"},
+            new Keyword { Id = 5, Name = "Equip", RusName = "Снарядить"},
+            new Keyword { Id = 6, Name = "First strike", RusName = "Первый удар"},
+            new Keyword { Id = 7, Name = "Flash", RusName = "Миг"},
+            new Keyword { Id = 8, Name = "Flying", RusName = "Полет"},
+            new Keyword { Id = 9, Name = "Haste", RusName = "Ускорение"},
+            new Keyword { Id = 10, Name = "Hexproof", RusName = "Порчеустойчивость"},
+            new Keyword { Id = 11, Name = "Indestructible", RusName = "Неразрушимость"},
+            new Keyword { Id = 12, Name = "Lifelink", RusName = "Цепь жизни"},
+            new Keyword { Id = 13, Name = "Menace", RusName = "Угроза"},
+            new Keyword { Id = 14, Name = "Protection", RusName = "Защита"},
+            new Keyword { Id = 15, Name = "Prowess", RusName = "Искусность"},
+            new Keyword { Id = 16, Name = "Reach", RusName = "Захват"},
+            new Keyword { Id = 17, Name = "Trample", RusName = "Пробивной удар"},
+            new Keyword { Id = 18, Name = "Vigilance", RusName = "Бдительность"}
+        );
+    }
 }
---
+++ MtgParser/Migrations/20220901181900_InitData.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MtgParser.Migrations
+{
+    public partial class InitData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Keywords",
+                columns: new[] { "Id", "Name", "RusName" },
+                values: new object[,]
+                {
+                    { 1, "Deathtouch", "Смертельное касание" },
+                    { 2, "Defender", "Защитник" },
+                    { 3, "Double strike", "Двойной удар" },
+                    { 4, "Enchant", "Зачаровать" },
+                    { 5, "Equip", "Снарядить" },
+                    { 6, "First strike", "Первый удар" },
+                    { 7, "Flash", "Миг" },
+                    { 8, "Flying", "Полет" },
+                    { 9, "Haste", "Ускорение" },
+                    { 10, "Hexproof", "Порчеустойчивость" },
+                    { 11, "Indestructible", "Неразрушимость" },
+                    { 12, "Lifelink", "Цепь жизни" },
+                    { 13, "Menace", "Угроза" },
+                    { 14, "Protection", "Защита" },
+                    { 15, "Prowess", "Искусность" },
+                    { 16, "Reach", "Захват" },
+                    { 17, "Trample", "" },
+                    { 18, "Vigilance", "Бдительность" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Rarities",
+                columns: new[] { "Id", "Name", "RusName" },
+                values: new object[,]
+                {
+                    { 1, "common", "обычная" },
+                    { 2, "uncommon", "необычная" },
+                    { 3, "rare", "редкая" },
+                    { 4, "mythic", "раритетная" },
+                    { 5, "special", "специальная" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Keywords",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Rarities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Rarities",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Rarities",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Rarities",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Rarities",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
