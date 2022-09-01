5a8dd7c8d823e169dd2a441324d8d7e77e397569
admin <111@111.ru>
2022-09-01 20:26:59 +0300

    add manual price migration

---
+++ MtgParser/Migrations/20220901172558_AddManualPrice.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MtgParser.Migrations
+{
+    public partial class AddManualPrice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ManualPrice",
+                table: "CardsSets",
+                type: "decimal(65,30)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ManualPrice",
+                table: "CardsSets");
+        }
+    }
+}
