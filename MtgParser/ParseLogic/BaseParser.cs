48c74d77e8f9bae5a76b3052cdc667ce418cc5d0
Aleksandr Senickin <aleksandr_senickin@senla.eu>
2022-09-09 13:31:12 +0300

    split parse logic to separate clases

---
+++ MtgParser/ParseLogic/BaseParser.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using AngleSharp;
+using AngleSharp.Dom;
+
+namespace MtgParser.ParseLogic;
+
+public class BaseParser
+{
+    public BaseParser()
+    {
+    }
+
+    public static (string main, string substr) GetSeparateString(string? source, string separator = "//")
+    {
+        if (string.IsNullOrEmpty(source))
+        {
+            throw new ArgumentException("source can't be null");
+        }
+        
+        int separatorIndex = source.IndexOf(separator, StringComparison.Ordinal);
+        if (separatorIndex < 0)
+        {
+            return (source, string.Empty);
+        }
+        
+        string left = source[..separatorIndex].Trim();
+        string right = source[separatorIndex..].Trim('/', ' ');
+        return left != right ? (left, right) : (left, string.Empty);
+    }
+
+    public async Task<IDocument> GetHtmlAsync(string path)
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        IConfiguration config = Configuration.Default.WithDefaultLoader();
+        IBrowsingContext context = BrowsingContext.New(config);
+        return await context.OpenAsync(path);
+    }
+
+    public static string GetSubStringAfterChar(string text, params char[] separators)
+    {
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (separators.Contains(text[i]))
+                return text[(i+1)..];
+        }
+
+        return text.Trim();
+    }
+}
